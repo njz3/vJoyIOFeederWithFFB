88f551f43f0d61417b22ef6f922b1fbf65020c3e
njz3 <benjamin.maurin@gmail.com>
2020-02-11 19:16:15 +0100

    Add new files

---
+++ vJoyIOFeeder/Configuration/RawAxisDB.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Windows;
+
+namespace vJoyIOFeeder.Configuration
+{
+    [Serializable]
+    public class RawAxisDB
+    {
+        public string vJoyAxis;
+        // For axis linear correction
+        public List<Point> ControlPoints;
+
+        public RawAxisDB()
+        {
+            vJoyAxis = null;
+            ControlPoints = new List<Point>();
+        }
+
+    }
+}
---
+++ vJoyIOFeeder/Configuration/RawInputDB.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Windows;
+
+namespace vJoyIOFeeder.Configuration
+{
+    [Serializable]
+    public class RawInputDB
+    {
+        public int[] vJoyBtns;
+        public bool IsInvertedLogic;
+        public bool IsToggle;
+        public bool IsAutoFire;
+
+        public RawInputDB()
+        {
+            vJoyBtns = new int[0];
+            IsInvertedLogic = false;
+            IsToggle = false;
+            IsAutoFire = false;
+        }
+
+    }
+}
